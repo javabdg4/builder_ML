9be70b07784bea04fe3fbabdce19f30a692d25c5
Mikis78 <mikis78@tlen.pl>
2018-08-23 19:30:16 +0200

    add builder

--- src/main/java/com/sda/builder/Main.java
+++ src/main/java/com/sda/builder/Main.java
@@ -2,6 +2,13 @@ package com.sda.builder;
 
 public class Main {
     public static void main(String[] args) {
+        User user =
+                User.UserBuilder.anUser()
+                .age(21)
+                .email("john@kovalsky.org")
+                .build();
 
+        System.out.println(user.getAge());
+        System.out.println(user.getEmail());
     }
 }
